c6f365dc8cd02925ccf9bdbbb9114e652b2e0f11
Petri Lautamäki <55832733+Rinssi@users.noreply.github.com>
2023-03-09 09:01:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Putkifirman nimi</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.html">Etusivu</a></li>
+          <li><a href="yhteystiedot.html">Yhteystiedot</a></li>
+          <li><a href="referenssit.html">Referenssit</a></li>
+        </ul>
+      </nav>
+    </header>
+    
+    <main>
+      <h1>Tervetuloa putkifirman nimi -sivustolle!</h1>
+      <p>Tarjoamme laadukkaita putkityötä, kuten...</p>
+      <ul>
+        <li>Putkien asennus</li>
+        <li>Putkien korjaus</li>
+        <li>Putkistojen puhdistus</li>
+        <li>Ja paljon muuta!</li>
+      </ul>
+    </main>
+    
+    <footer>
+      <p>Putkifirman nimi | Yhteystiedot: 123 456 789 | Sähköposti: putkifirma@esimerkki.fi</p>
+    </footer>
+  </body>
+</html>
